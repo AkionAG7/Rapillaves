e842d9cf9db5f74ad88227733da6fab9603270a4
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-12-13 23:29:47 -0600

    Conf: Primera parte de la configuracion

---
+++ my_project/rapillaves/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RapillavesConfig(AppConfig):
+    name = 'rapillaves'
---
+++ my_project/rapillaves/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Proveedor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank='N/A')
+    number_phone = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name 
+
+class Product_Provedor(models.Model):
+    product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    proveedor = models.ForeignKey(Proveedor, on_delete= models.CASCADE)
+    
+    def __str__(self):
+        return self.pk
+    
+class Operation(models.Model):
+    operation_type = models.BooleanField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    
+    def __str__(self):
+        return self.pk
+
+
