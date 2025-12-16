96218d201b704a083623daee38e94cf25577101b
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-12-15 18:11:44 -0600

    Feat: Vista de proveedores

--- my_project/rapillaves/urls.py
+++ my_project/rapillaves/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import show_inventory
+from .views import show_inventory, show_proveedores
 
 urlpatterns = [
-    path('inventario/', show_inventory, name='inventario')
+    path('inventario/', show_inventory, name='inventario'),
+    path('proveedores/', show_proveedores, name='proveedores')
 ]
--- my_project/rapillaves/views.py
+++ my_project/rapillaves/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Proveedor
 
 def show_inventory(request):
     productos = Product.objects.all()
     context = {"productos" : productos}
     return render(request ,'inventory/inventory.html', context)
+
+def show_proveedores(request):
+    proveedores = Proveedor.objects.all()
+    context = {"proveedores": proveedores}
+    return render(request, "inventory/proveedor.html", context)
