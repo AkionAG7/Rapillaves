b4badba6b799d3e94ace60ce9fefb150c8348fbe
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-12-16 19:32:42 -0600

    Feat: Funcionalidad de editar y ver info completa de inventario

--- my_project/rapillaves/urls.py
+++ my_project/rapillaves/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import show_inventory, show_proveedores, show_operation_options, create_inventory
+from .views import show_inventory, show_proveedores, show_operation_options, create_inventory, update_inventory,show_product
 
 urlpatterns = [
     path('inventario/', show_inventory, name='inventario'),
+    path('inventario/<int:pk>', show_product, name='producto'),
     path('inventario/create/', create_inventory, name='inventory_create'),
+    path('inventario/update/<int:pk>/', update_inventory, name='inventory_update'),
     path('proveedores/', show_proveedores, name='proveedores'),
     path('operaciones/', show_operation_options, name='operation_options')
 ]
--- my_project/rapillaves/views.py
+++ my_project/rapillaves/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Product, Proveedor
 from .forms import ProductForm
 
 def show_inventory(request):
     productos = Product.objects.all()
     form = ProductForm()
-    context = {"productos" : productos, "form": form}
+    context = {"productos" : productos, "form": form, "edit_mode" : False, "show_mode": False}
     return render(request ,'inventory/inventory.html', context)
+
+def show_product(request, pk):
+    productos = Product.objects.all()
+    producto = get_object_or_404(Product, pk = pk)
+    return render(request, "inventory/inventory.html",{
+        "productos" : productos,
+        "form": ProductForm(instance=producto),
+        "edit_mode" : False,
+        "show_mode" : True,
+        "producto" : producto
+    })
 
 def create_inventory(request):
     if request.method == "POST":
@@ -14,6 +25,25 @@ def create_inventory(request):
         if form.is_valid():
             form.save()
     return redirect("inventario")
+
+def update_inventory(request, pk):
+    producto = get_object_or_404(Product, pk = pk)
+    
+    if request.method == "POST" :
+        form = ProductForm(request.POST, instance= producto)
+        if form.is_valid():
+            form.save()
+            return redirect("inventario")
+    else:
+        form = ProductForm(instance=producto)
+    productos = Product.objects.all()
+    return render(request, "inventory/inventory.html",{
+        "productos" : productos,
+        "form" : form,
+        "edit_mode" : True,
+        "show_mode" : False,
+        "producto" : producto
+    })
 
 def show_proveedores(request):
     proveedores = Proveedor.objects.all()
