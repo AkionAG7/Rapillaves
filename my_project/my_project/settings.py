b7fa3e0f3f335d4cf343267452e2aee84a076e9d
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-12-19 11:00:23 -0600

    Style: estilo en formularios y correciones

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rapillaves'
+    'rapillaves',
+    'django_icons'
 ]
 
 MIDDLEWARE = [
